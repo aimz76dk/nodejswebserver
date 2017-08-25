cd30a488f545eefacd355e65c1542ed7ffbf7c6f
aimz <aimzbooking@gmail.com>
2017-08-25 13:29:17 +0200

    heroku ready

---
+++ server.js
@@ -0,0 +1,16 @@
+  
+  var http = require('http');
+
+    var server = http.createServer(function (req, res) {
+      res.writeHead(200, {'Content-Type': 'text/json'});
+      res.end(`{ "title" : "Hello World", "Text" : "This is my first web api" }`);
+    });
+
+    // server.listen(3000);
+
+    server.listen(process.env.PORT || 5000);
+
+
+
+
+
