5072e3a1fb29adefb1b08d9a7e155c1773370181
aimz <aimzbooking@gmail.com>
2017-09-08 09:39:50 +0200

    h

--- server.js
+++ server.js
@@ -1,16 +1,13 @@
-  
-  var http = require('http');
+var http = require('http');
 
-    var server = http.createServer(function (req, res) {
-      res.writeHead(200, {'Content-Type': 'text/json'});
-      res.write(`{ "title" : "Hello World", "Text" : "This is my first web api" }`);
-      res.end();
-    }); 
+var server = http.createServer(function(req, res){
+    
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    
+    res.writeHead(200, {'Content-type' : 'text/json'});
+    res.write('{"title" : "Hello JSON web API", "text" : "Text fra min API"}');
+    res.end();
+});
 
-
-    server.listen(process.env.PORT || 5000);
-
-
-
-
+server.listen(process.env.PORT || 3003);
 
