bbd999ed2cf2944dc9ee0546578ad78a14ddf495
aimz <aimzbooking@gmail.com>
2017-09-08 11:27:29 +0200

    read file function

--- server.js
+++ server.js
@@ -1,15 +1,13 @@
 var http = require('http');
 var fs = require('fs');
 
-
-
 var server = http.createServer(function(req, res) {
     
     res.setHeader('Access-Control-Allow-Origin', '*');
-    
-    res.writeHead(200, {'Content-type' : 'text/json'});
-    res.write(fs.readFile('index.html', 'utf-8'));
-    res.end();
+    res.writeHead(200, {'Content-type' : 'text/html'})
+    fs.readFile('index.html', 'utf-8', function (err, data) {
+        res.end(data.toString());
+    });   
 });
 
 server.listen(process.env.PORT || 3003);
